19f6151cd0e2b4e55639fdaedec09218a1b1f1bc
tamharo <117295948+tamharo@users.noreply.github.com>
2022-12-04 14:59:00 +0100

    remove sh file

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def say_hello():
-    return "Goliv API v1.0.10"
-
+    return "Goliv API v1.0.11"
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
--- teardown.sh
+++
@@ -1,9 +0,0 @@
-set -x
-
-MY_INSTANCE_NAME="manhamprod"
-ZONE=europe-west1-b
-
-gcloud compute instances delete $MY_INSTANCE_NAME \
-    --zone=$ZONE --delete-disks=all
-
-gcloud compute firewall-rules delete default-allow-http-8080
