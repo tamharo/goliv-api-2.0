a380194366c16ee3aaf182d1e0fbcad291c3d532
tamharo <117295948+tamharo@users.noreply.github.com>
2022-12-04 13:06:22 +0100

    initial

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,5 @@
-# Copyright 2019 Google LLC All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 set -ex
 
-# [START getting_started_gce_create_instance]
 MY_INSTANCE_NAME="manhamprod"
 ZONE=europe-west1-b
 
@@ -26,7 +11,6 @@ gcloud compute instances create $MY_INSTANCE_NAME \
     --metadata-from-file startup-script=startup-script.sh \
     --zone $ZONE \
     --tags http-server
-# [END getting_started_gce_create_instance]
 
 gcloud compute firewall-rules create default-allow-http-8080 \
     --allow tcp:8080 \
